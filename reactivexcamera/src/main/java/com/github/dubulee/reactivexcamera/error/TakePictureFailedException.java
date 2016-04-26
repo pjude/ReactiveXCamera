d36b0a55b8c25a5486e4e14247d9fbad39b21819
mugku <mugkulee@gmail.com>
2016-04-26 13:50:03 +0900

    Update TakePictureFailedException.java

--- reactivexcamera/src/main/java/com/github/dubulee/reactivexcamera/error/TakePictureFailedException.java
+++ reactivexcamera/src/main/java/com/github/dubulee/reactivexcamera/error/TakePictureFailedException.java
@@ -1,7 +1,7 @@
 package com.github.dubulee.reactivexcamera.error;
 
 /**
- * Created by dubulee on 15/12/23.
+ * Created by mugku on 15/12/23.
  */
 public class TakePictureFailedException extends Exception {
     public TakePictureFailedException(String detailMessage) {
