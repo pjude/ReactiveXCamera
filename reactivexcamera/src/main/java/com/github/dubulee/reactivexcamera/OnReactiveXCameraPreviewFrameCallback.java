10c7ea4e937cf38051e98b5cd671da65134a52eb
mugku <mugkulee@gmail.com>
2016-04-26 13:46:06 +0900

    Update OnReactiveXCameraPreviewFrameCallback.java

--- reactivexcamera/src/main/java/com/github/dubulee/reactivexcamera/OnReactiveXCameraPreviewFrameCallback.java
+++ reactivexcamera/src/main/java/com/github/dubulee/reactivexcamera/OnReactiveXCameraPreviewFrameCallback.java
@@ -1,7 +1,7 @@
 package com.github.dubulee.reactivexcamera;
 
 /**
- * Created by dubulee on 15/12/23.
+ * Created by mugku on 15/12/23.
  */
 public interface OnReactiveXCameraPreviewFrameCallback {
     void onPreviewFrame(byte[] data);
